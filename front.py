b76ade5de275252ec2e027d2b1d1036ba3e09e3e
hardope <adeyeriopeoluwa05@gmail.com>
2024-12-17 23:26:09 +0100

    web-app setup

--- front.py
+++ front.py
@@ -3,70 +3,99 @@ import requests
 
 API_BASE_URL = "http://localhost:8000"  # Update with your FastAPI URL
 
-st.title("Secure .env File Sharing")
+st.title("Secure Credentials File Sharing")
 
-# Upload Section
-st.header("Upload .env File")
-uploaded_file = st.file_uploader("Choose your file", type=["env", "py", "txt", "*"])
+# Main menu
+option = st.selectbox("Choose an option", ["Select Action Upload / Download","Create Credential File", "Download Credential File"])
 
-if uploaded_file is not None:
-    if st.button("Upload File"):
-        try:
-            files = {"file": (uploaded_file.name, uploaded_file.getvalue())}
-            response = requests.post(f"{API_BASE_URL}/upload", files=files)
-            if response.status_code == 200:
-                result = response.json()
-                download_code = result.get("download_code")
-                decryption_key = result.get("decryption_key")
-                st.success(f"File uploaded successfully! Your download code is: {download_code}")
-                st.code(decryption_key, language="plaintext")
-                st.info("Save this decryption key securely. You will need it to download your file.")
-            else:
-                st.error(f"Failed to upload file: {response.json().get('detail', 'Unknown error')}")
-        except Exception as e:
-            st.error(f"An error occurred: {str(e)}")
+if option == "Create Credential File":
+    # Upload Section
+    st.header("Upload Credential File")
+    uploaded_file = st.file_uploader("Choose your file", type=["env", "txt", "*", "json"])
 
-# Upload Text Section
-st.header("Upload .env Text Content")
-env_text_content = st.text_area("Enter your .env file content here")
+    download_limit = st.number_input("Download Limit", value=1, min_value=1, step=1)
+    expiration_time = st.number_input("Expiration Time (in minutes)", value=5, min_value=1, step=1)
 
-if env_text_content:
-    if st.button("Upload Text Content"):
-        try:
-            response = requests.post(f"{API_BASE_URL}/upload_text", data={"file_content": env_text_content})
-            if response.status_code == 200:
-                result = response.json()
-                download_code = result.get("download_code")
-                decryption_key = result.get("decryption_key")
-                st.success(f"Text content uploaded successfully! Your download code is: {download_code}")
-                st.code(decryption_key, language="plaintext")
-                st.info("Save this decryption key securely. You will need it to download your file.")
-            else:
-                st.error(f"Failed to upload text content: {response.json().get('detail', 'Unknown error')}")
-        except Exception as e:
-            st.error(f"An error occurred: {str(e)}")
+    if uploaded_file is not None:
+        if st.button("Upload File"):
+            try:
+                files = {"file": (uploaded_file.name, uploaded_file.getvalue())}
+                response = requests.post(f"{API_BASE_URL}/upload", files=files, data = {
+                    "download_limit": download_limit,
+                    "expiration_time": expiration_time
+                })
+                if response.status_code == 200:
+                    result = response.json()
+                    download_code = result.get("download_code")
+                    decryption_key = result.get("decryption_key")
+                    st.success(f"File uploaded successfully! Find your download code and decryption key below.")
+                    st.write("Download Code")
+                    st.code(download_code, language="plaintext")
+                    st.write("Decryption Key")
+                    st.code(decryption_key, language="plaintext")
+                    st.info("Save this decryption key securely. You will need it to download your file.")
+                else:
+                    st.error(f"Failed to upload file: {response.json().get('detail', 'Unknown error')}")
+            except Exception as e:
+                st.error(f"An error occurred: {str(e)}")
 
-# Download Section
-st.header("Download .env File")
-download_code = st.text_input("Enter your download code")
-decryption_key = st.text_input("Enter your decryption key")
+    # Upload Text Section
+    st.header("Upload Credentials Text Content")
+    env_text_content = st.text_area("Enter your credentials file content here")
 
-if st.button("Download File"):
-    if download_code and decryption_key:
-        try:
-            params = {"decryption_key": decryption_key}
-            response = requests.get(f"{API_BASE_URL}/download/{download_code}", params=params, stream=True)
-            if response.status_code == 200:
-                st.success("File downloaded successfully!")
-                st.download_button(
-                    label="Download .env file",
-                    data=response.content,
-                    file_name="env_file.env",
-                    mime="application/octet-stream"
-                )
-            else:
-                st.error(f"Failed to download file: {response.json().get('detail', 'Unknown error')}")
-        except Exception as e:
-            st.error(f"An error occurred: {str(e)}")
-    else:
-        st.error("Please enter both a valid download code and decryption key.")
+    if env_text_content:
+        if st.button("Upload Text Content"):
+            try:
+                response = requests.post(f"{API_BASE_URL}/upload_text", data={
+                    "file_content": env_text_content,
+                    "download_limit": download_limit,
+                    "expiration_time": expiration_time
+
+                })
+                if response.status_code == 200:
+                    result = response.json()
+                    download_code = result.get("download_code")
+                    decryption_key = result.get("decryption_key")
+                    st.success(f"File uploaded successfully! Find your download code and decryption key below.")
+                    st.write("Download Code")
+                    st.code(download_code, language="plaintext")
+                    st.write("Decryption Key")
+                    st.code(decryption_key, language="plaintext")
+                    st.info("Save this decryption key securely. You will need it to download your file.")
+                else:
+                    st.error(f"Failed to upload text content: {response.json().get('detail', 'Unknown error')}")
+            except Exception as e:
+                st.error(f"An error occurred: {str(e)}")
+
+elif option == "Download Credential File":
+    # Download Section
+    st.header("Download Credential File")
+    download_code = st.text_input("Enter your download code")
+    decryption_key = st.text_input("Enter your decryption key")
+
+    if st.button("Download File"):
+        if download_code and decryption_key:
+            try:
+                params = {"decryption_key": decryption_key}
+                response = requests.get(f"{API_BASE_URL}/download/{download_code}", params=params, stream=True)
+                if response.status_code == 200:
+                    st.success("File downloaded successfully!")
+                    st.download_button(
+                        label="Download Credemtials file",
+                        data=response.content,
+                        file_name="credentials.txt",
+                        mime="application/octet-stream"
+                    )
+                else:
+                    st.error(f"Failed to download file: {response.json().get('detail', 'Unknown error')}")
+            except Exception as e:
+                st.error(f"An error occurred: {str(e)}")
+        else:
+            st.error("Please enter both a valid download code and decryption key.")
+
+else:
+
+    st.header("Select Action Upload / Download")
+    st.write("This web app allows you to securely share env files or credentials with others. "
+             "You can either upload a file or enter the content directly, and you will receive a download code and decryption key. "
+             "To download a file, provide the download code and decryption key.")
--- main.py
+++ main.py
@@ -5,7 +5,7 @@ from cryptography.fernet import Fernet
 import io
 import os
 import uuid
-from sqlalchemy import create_engine, Column, String, DateTime
+from sqlalchemy import Integer, create_engine, Column, String, DateTime
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker, Session
 from datetime import datetime
@@ -30,6 +30,8 @@ class EnvFile(Base):
     __tablename__ = "env_files"
     id = Column(String, primary_key=True, index=True)
     encrypted_data = Column(String, nullable=False)
+    download_limit = Column(Integer, nullable=True)
+    expiration_time = Column(Integer, nullable=True)
     created_at = Column(DateTime, default=datetime.utcnow)
 
 # FastAPI app setup
@@ -45,7 +47,7 @@ class UploadResponse(BaseModel):
 
 
 @app.post("/upload", response_model=UploadResponse)
-def upload_env(file: UploadFile, download_limit: int = 1, expiration_time: int = 5, db: Session = Depends(get_db)):
+def upload_env(file: UploadFile, download_limit: int = Form(1), expiration_time: int = Form(5), db: Session = Depends(get_db)):
     try:
         file_content = file.file.read()
         decryption_key = Fernet.generate_key()
@@ -53,26 +55,26 @@ def upload_env(file: UploadFile, download_limit: int = 1, expiration_time: int =
         encrypted_data = fernet.encrypt(file_content)
 
         download_code = str(uuid.uuid4())
-        env_file = EnvFile(id=download_code, encrypted_data=encrypted_data.decode())
+        env_file = EnvFile(id=download_code, encrypted_data=encrypted_data.decode(), download_limit=download_limit, expiration_time=expiration_time)
 
         db.add(env_file)
         db.commit()
 
-        print(f"Download code: {download_limit}, Expiration time: {expiration_time}")
+        print(f"Download Limit: {download_limit}, Expiration time: {expiration_time}")
 
         return {"download_code": download_code, "decryption_key": decryption_key.decode()}
     except Exception as e:
         raise HTTPException(status_code=500, detail=f"Error uploading file: {str(e)}")
 
 @app.post("/upload_text", response_model=UploadResponse)
-def upload_env_text(file_content: str = Form(...), db: Session = Depends(get_db)):
+def upload_env_text(file_content: str = Form(...), download_limit: int = Form(1), expiration_time: int = Form(5), db: Session = Depends(get_db)):
     try:
         decryption_key = Fernet.generate_key()
         fernet = Fernet(decryption_key)
         encrypted_data = fernet.encrypt(file_content.encode())
 
         download_code = str(uuid.uuid4())
-        env_file = EnvFile(id=download_code, encrypted_data=encrypted_data.decode())
+        env_file = EnvFile(id=download_code, encrypted_data=encrypted_data.decode(), download_limit=download_limit, expiration_time=expiration_time)
 
         db.add(env_file)
         db.commit()
@@ -88,9 +90,24 @@ def download_env(code: str, decryption_key: str, db: Session = Depends(get_db)):
     if not env_file:
         raise HTTPException(status_code=404, detail="File not found")
 
+    # Check download limit
+    if env_file.download_limit is not None:
+        if env_file.download_limit <= 0:
+            raise HTTPException(status_code=403, detail="Download limit exceeded")
+        env_file.download_limit -= 1
+        db.commit()
+
+    # Check expiration time
+    if env_file.expiration_time is not None:
+        time_elapsed = (datetime.utcnow() - env_file.created_at).total_seconds() / 60
+        print(f"Time elapsed: {time_elapsed}")
+        if time_elapsed > env_file.expiration_time:
+            raise HTTPException(status_code=403, detail="File has expired")
+
     try:
         fernet = Fernet(decryption_key.encode())
         decrypted_data = fernet.decrypt(env_file.encrypted_data.encode())
+        db.commit()
     except Exception as e:
         raise HTTPException(status_code=400, detail="Invalid decryption key")
 
