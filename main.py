86db2d5825ec950f5c796e96b933cc401619f267
hardope <adeyeriopeoluwa05@gmail.com>
2024-12-23 08:55:08 +0100

    hotfix

--- main.py
+++ main.py
@@ -118,4 +118,4 @@ def download_env(code: str, decryption_key: str, db: Session = Depends(get_db)):
 
 if __name__ == "__main__":
     import uvicorn
-    uvicorn.run(app, host="127.0.0.1", port=8000)
+    uvicorn.run(app, host="0.0.0.0", port=10000)
